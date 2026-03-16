8a264e768eb549ae30c8a73a965c952c8c0ba8c5
Digital Marketing <digital@truaxmarketing.com>
2026-03-16 21:07:05 +0000

    File changes

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,8 +3,10 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
-} 
-
+}
 
 export const isIframe = window.self !== window.top;
-export function isInIframe() { return window.self !== window.top; }
+
+export function isInIframe() {
+  return window.self !== window.top;
+}
